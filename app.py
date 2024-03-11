3db4520657c390fea9e42cd8f95feb6ea940c668
Samantha Estudillo <samm.lestudillo@gmail.com>
2024-03-11 12:35:13 -0600

    Casillas de verificación

--- app.py
+++ app.py
@@ -5,25 +5,25 @@ import streamlit as st
 st.header('Título pendiente')
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
+
+show_histogram = st.checkbox('Construir histograma') # crear una casilla de verificación para el histograma
+show_scatter = st.checkbox('Construir gráfico de dispersión') # crear una casilla de verificación para el gráfico de dispersión
         
-if hist_button: # al hacer clic en el botón
+if show_histogram: # si la casilla de verificación del histograma está marcada
     # escribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
             
     # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
+    fig = px.histogram(car_data, x="Odómetro", color_discrete_sequence=['red'])
         
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
-
-gdisp_button = st.button('Construir gráfico de dispersión') # crear un botón
-        
-if gdisp_button: # al hacer clic en el botón
+       
+if show_scatter: # si la casilla de verificación del gráfico de dispersión está marcada
     # escribir un mensaje
     st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
             
-    fig = px.scatter(car_data, x="odometer", y="price")
+    fig = px.scatter(car_data, x="Odómetro", y="Precio", color_discrete_sequence=['orange'])
         
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
